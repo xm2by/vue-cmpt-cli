ea28de761318358d37f2548fb1dc055dbc08e123
徐敏 <xumin@xumindeMBP.lan>
2019-07-28 12:48:31 +0800

    feat: 新增脚手架初始化项目代码

---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+#!usr/bin/env node
+
+const program = require('commander')
+
+program
+  .version(require('../package.json').version)
+  .usage('<command>')
+
+program
+  .command('init')
+  .description('create a new project')
+  .action(() => {
+    require('../lib/init.js')
+  })
+
+program.parse(process.argv)
+if (!program.args.length) {
+  program.help()
+}
